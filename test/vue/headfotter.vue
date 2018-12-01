d450e335a690f23594bc0a3c2a5785bf54ed2ef4
zhannanana <45509389+zhannanana@users.noreply.github.com>
2018-12-01 17:30:12 +0800

    summary

---
+++ test/vue/headfotter.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="tnavbar">
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">智慧城市计算数据展示平台</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active"><a href="traffic-index.html">{{traffic}}<span class="sr-only">(current)</span></a></li>
+                        <li><a href="../meteorology/meteorology-index.html">{{weather}}</a></li>
+                        <li><a href="../hydrology/hydrology-index.html">{{hydrology}} </a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </nav>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'tnavbar',
+        data () {
+            return {
+                msg: '欢迎来到菜鸟教程！'
+            }
+        }
+    }
+</script>
---
+++ test/vue/vuetest.vue
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Dashboard Template for Bootstrap</title>
+    <!-- 引入 ECharts 文件 -->
+    <script src="../../js/echarts.min.js"></script>
+    <script  src="../../js/jquery.min.js"></script>
+    <script src="https://cdn.staticfile.org/vue/2.2.2/vue.min.js"></script>
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../../css/dashboard.css" rel="stylesheet">
+    <link href="../../css/theme.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <template>
+        <div id="app">
+            <tnavbar></tnavbar>
+        </div>
+    </template>
+
+<script type="text/javascript">
+        import tnavbar from './test/vue/headfotter.vue'
+        export default {
+            name: 'app',
+            components: {
+                tnavbar
+            }
+        }
+    </script>
+</body>
+</html>
